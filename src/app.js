89c370e72d788f42e132958d43bec670d00ea34a
Konstantin Kononenko <kalikrit@gmail.com>
2017-04-14 15:14:45 +0300

    configuration

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    static path = '/';
+
+    render() {
+        return (
+            <h2>Мое первое приложение на React</h2>
+        );
+    }
+}
