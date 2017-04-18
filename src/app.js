325a0ada1e1b031e9d0d9a5756edefa94a1bcdf2
Konstantin Kononenko <kalikrit@gmail.com>
2017-04-18 21:22:41 +0300

    devtools configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { DevTools } from './utils/index';
 
 export default class App extends React.Component {
 
@@ -6,7 +7,10 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <h2>Мое первое приложение на React</h2>
+            <div>
+                <h2>Мое первое приложение на React</h2>
+                [ process.env.MODE_DEV !== 'production' ? <DevTools/> : null]
+            </div>    
         );
     }
 }
